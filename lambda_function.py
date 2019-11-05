817ec8d688f9fe39c50ce734f7c92c021694b6ee
marthayq <43948233+marthayq@users.noreply.github.com>
2019-11-06 00:43:49 +0800

    Update lambda_function.py
    progress bar,
    status check,
    response to user

--- lambda_function.py
+++ lambda_function.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     
     with open('index.html', 'r') as f:
         indexPage = f.read()
-        
+    status_check = [0]*10 
     
     if method == 'GET':
         return {
@@ -32,24 +32,33 @@ def lambda_handler(event, context):
         parsedBodyContent = json.loads(bodyContent)
         testCases = re.sub('&zwnj;.*&zwnj;','',parsedBodyContent["shown"]["0"], flags=re.DOTALL) 
         userSolution = parsedBodyContent["editable"]["0"] 
-        
+        questionName = parsedBodyContent["qname"]["0"]
         original_df = pd.read_csv('https://frame-pandas.s3.amazonaws.com/pandas_data.csv')
         user_output_df = pd.eval(userSolution)
         userHtmlFeedback = user_output_df.to_html()
         
         # Question 1
+        isComplete = 0
         right_answer = original_df[original_df['GENDER']=='F']
-        right_answer.equals(user_output_df)
-        
+        right_answer_text = 'original_df[original_df[\'GENDER\']==\'F\']'
+        if(right_answer.equals(user_output_df)):
+            status_check[0]=1
+            isComplete = 1
+
+
+        progress = len([qn for qn in status_check if qn == 1])
+        print(progress)
         return {
             "statusCode": 200,
             "headers": {
             "Content-Type": "application/json",
                 },
             "body":  json.dumps({
-                "isComplete":"Hello",
+                "isComplete":isComplete,
                 "pythonFeedback": "Hello",
                 "htmlFeedback": userHtmlFeedback,
-                "textFeedback": "Hello"
+                "textFeedback": right_answer_text,
+                "progress": progress,
+                "questionStatus":status_check
             })
         }
