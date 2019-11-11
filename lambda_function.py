2aac4145420e41501445124130f386bc08dd4d3f
Naheom <32996873+Naheom@users.noreply.github.com>
2019-11-11 17:06:16 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -28,10 +28,10 @@ def lambda_handler(event, context):
     logger.addHandler(ch)
     
     def runCode(myDF, myCode):
-        namespace = {'original_df': myDF}
+        namespace = {'original_df': myDF, 'expected_output': ''}
         exec('import pandas as pd', namespace)
         exec(myCode, namespace)
-        return namespace['original_df']
+        return namespace['expected_output']
     
     s3 = bt3.client('s3')
     method = event.get('httpMethod',{}) 
@@ -63,19 +63,21 @@ def lambda_handler(event, context):
         errorStatus = False
         # Evaluating User Inputs
         try:
-            original_df = runCode(original_df, userSolution)
+            expected_output = runCode(original_df, userSolution)
         except:
             errorStatus = True
             logger.exception('Debug Message')
+            
+        print("Hello World", expected_output)
         
         if not errorStatus:
-            if isinstance(original_df, str):
-                userHtmlFeedback = original_df
-            elif isinstance(original_df, pd.core.series.Series):
-                original_df = original_df.to_frame()
-                userHtmlFeedback = original_df.to_html()
+            if isinstance(expected_output , str):
+                userHtmlFeedback = expected_output 
+            elif isinstance(expected_output, pd.core.series.Series):
+                expected_output = expected_output.to_frame()
+                userHtmlFeedback = expected_output.to_html()
             else:
-                userHtmlFeedback = original_df.to_html()
+                userHtmlFeedback = expected_output.to_html()
         else:
             userHtmlFeedback = "Error - Please Look at Python Logs"
         
@@ -85,61 +87,61 @@ def lambda_handler(event, context):
         if questionName == 'Selecting Rows': #Q1
             right_answer = default_df.iloc[[1,4,9]]
             right_answer_text = 'original_df.iloc[[1,4,9]]'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[0]=1
                 isComplete = 1
         elif questionName == 'Selecting Columns':#Q2
             right_answer = default_df[['ID','NAME','RESIDENCE']]
             right_answer_text = 'original_df[[\'ID\',\'NAME\',\'RESIDENCE\']]'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[1]=1
                 isComplete = 1
         elif questionName == 'Selecting Specific Cells':#Q3
             right_answer = default_df.iloc[8]['NAME']
             right_answer_text = 'original_df.iloc[8][\'NAME\']'
-            if(right_answer==(original_df)):
+            if(right_answer==(expected_output)):
                 status_check[2]=1
                 isComplete = 1
         elif questionName == 'Replacing String Occurrences':#Q4
             right_answer = default_df.replace("USA","United States")
             right_answer_text = 'original_df.replace("USA","United States")'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[3]=1
                 isComplete = 1
         elif questionName == 'Filtering Data in Columns':#Q5
             right_answer = default_df[default_df['CHILDREN']=='Yes']
             right_answer_text = 'original_df[original_df[\'CHILDREN\']==\'Yes\']'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[4]=1
                 isComplete = 1
         elif questionName == 'Filtering Data based on Multiple Conditions':#Q6
             right_answer = default_df[(original_df['CHILDREN']=='Yes')|(original_df['RESIDENCE']=='China')]
             right_answer_text = 'original_df[(original_df[\'CHILDREN\']==\'Yes\')|(original_df[\'RESIDENCE\']==\'China\')]'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[5]=1
                 isComplete = 1
         elif questionName == 'Adding Rows':#Q7
             right_answer = default_df.append(pd.Series([11,'Tao Tao', 'M', 20, 'Yes', 'Singapore'], index=original_df.columns), ignore_index=True)
             right_answer_text = 'original_df.append(pd.Series([11,\'Tao Tao\', \'M\', 20, \'Yes\', \'Singapore\'], index=original_df.columns), ignore_index=True)'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[6]=1
                 isComplete = 1
         elif questionName == 'Deleting Rows':#Q8
             right_answer = default_df.drop(original_df.index[[4,9]])
             right_answer_text = 'original_df.drop(original_df.index[[4,9]])'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[7]=1
                 isComplete = 1
         elif questionName == 'Finding Min and Max':#Q9
             right_answer = default_df['NAME'].min()
             right_answer_text = 'original_df[\'NAME\'].min()'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[8]=1
                 isComplete = 1
         elif questionName == 'Multiplying and Dividing Column Values':#Q10
             right_answer = 10*default_df.iloc[:,3]
             right_answer_text = '10*original_df.iloc[:,3]'
-            if(right_answer.equals(original_df)):
+            if(right_answer.equals(expected_output)):
                 status_check[9]=1
                 isComplete = 1
         
@@ -159,7 +161,7 @@ def lambda_handler(event, context):
             "body":  json.dumps({
                 "isComplete":isComplete,
                 "pythonFeedback": log_contents.lower(),
-                "htmlFeedback": userHtmlFeedback,
+                "htmlFeedback": right_answer_text + "\n" + userHtmlFeedback,
                 "textFeedback": right_answer_text,
                 "progress": progress,
                 "questionStatus":status_check
